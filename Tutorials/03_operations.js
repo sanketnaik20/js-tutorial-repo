b68cf76214629e984e8091716f8b0fecc70182d6
sanketnaik20 <64030169+sanketnaik20@users.noreply.github.com>
2023-09-29 15:17:22 +0000

    Changed Name of Folder And Learnt Conversion

---
+++ Tutorials/03_operations.js
@@ -0,0 +1,26 @@
+let score=undefined
+
+// console.log(typeof score);
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"33"=> 33
+//"33abc"=> NaN
+//true=> 1
+//false=>0
+
+
+let isLoggedIn=1
+console.log(typeof isLoggedIn);
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log( booleanIsLoggedIn);
+
+
+//true=>1
+//false =>0
+// ""=false
+//sanket=true
