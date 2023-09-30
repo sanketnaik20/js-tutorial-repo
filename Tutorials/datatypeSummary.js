722e226476362262b731c6a9f1bfa7fa5fa7b011
sanketnaik20 <64030169+sanketnaik20@users.noreply.github.com>
2023-09-30 15:17:51 +0000

    stack and heap memory

--- Tutorials/datatypeSummary.js
+++ Tutorials/datatypeSummary.js
@@ -3,32 +3,60 @@
 
 // 7 types : String,Number,Boloean,null,undefined,Symbol,BigInt
 
-const score=100
-const anotherScore=100.3
+// const score=100
+// const anotherScore=100.3
 
-const isLoggedIn=true
-const outsideTemp=null
+// const isLoggedIn=true
+// const outsideTemp=null
 
-let userEmailId
+// let userEmailId
 
-const id=Symbol('190')
-const anotherId=Symbol('190')
-// console.log(id===anotherId);
+// const id=Symbol('190')
+// const anotherId=Symbol('190')
+// // console.log(id===anotherId);
 
 
-const bigNumber=82292984928248n
+// const bigNumber=82292984928248n
 
-//Non- Primitive ( Reference)
-//Arrays,Objects,functions
+// //Non- Primitive ( Reference)
+// //Arrays,Objects,functions
 
-const Heros=['shaktiman','ironman','golu'];
+// const Heros=['shaktiman','ironman','golu'];
 
-let myObj={
-    name:"Sanket",
-    age:19,
+// let myObj={
+//     name:"Sanket",
+//     age:19,
+// }
+
+
+// const myFucntion = function(){
+//     console.log("Hello world");
+// }
+
+
+//++++++++++++++++++++++++++++++++++//
+
+
+//stack(primitive), heap(non primitive)
+
+
+let myYoutubeName="sanket naik";
+
+let YoutubeName=myYoutubeName;
+console.log(YoutubeName);
+
+
+
+let userOne={
+    email:"sanket@rediff.com",
+
+    upi: "sanket@upi"
 }
+let userTwo=userOne;
+
+userTwo.email="naiksanket@mail";
+console.log(userTwo.email);
+console.log(userOne.email);
 
 
-const myFucntion = function(){
-    console.log("Hello world");
-}
+
