30647aab012bf5d294d3ffd3cefe79ee9ab2d5a1
sanketnaik20 <64030169+sanketnaik20@users.noreply.github.com>
2023-09-30 14:02:18 +0000

    learnt opertions

--- Tutorials/03_operations.js
+++ Tutorials/03_operations.js
@@ -13,14 +13,52 @@ let score=undefined
 //false=>0
 
 
-let isLoggedIn=1
-console.log(typeof isLoggedIn);
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(typeof booleanIsLoggedIn);
-console.log( booleanIsLoggedIn);
+// let isLoggedIn=1
+// console.log(typeof isLoggedIn);
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn);
+// console.log( booleanIsLoggedIn);
 
 
 //true=>1
 //false =>0
 // ""=false
 //sanket=true
+
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+///* Opreations*//////
+
+let value=3
+let negValue=-value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1="Sanket"
+// let str2=" Naik"
+// let str3=str1+str2;
+// console.log(str3);
+
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log((2+3)*(5%9));
+
+// console.log(true);
+
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);
