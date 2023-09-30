1ea647eb16ca84613e39b1fd4496d788d7d2f48c
sanketnaik20 <64030169+sanketnaik20@users.noreply.github.com>
2023-09-30 14:11:12 +0000

    Comparisons

---
+++ Tutorials/04_comparison.js
@@ -0,0 +1,10 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2!=1);
+// console.log(2==1);
+
+console.log(null>0);
+//=== it checks datatype too
+console.log(2===2);
