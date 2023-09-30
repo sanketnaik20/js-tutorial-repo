5efb9ddf12511300436dc4c928c1352d10fa9b14
sanketnaik20 <64030169+sanketnaik20@users.noreply.github.com>
2023-09-30 14:54:24 +0000

    summary of data types

---
+++ Tutorials/datatypeSummary.js
@@ -0,0 +1,34 @@
+//# Primitive
+
+
+// 7 types : String,Number,Boloean,null,undefined,Symbol,BigInt
+
+const score=100
+const anotherScore=100.3
+
+const isLoggedIn=true
+const outsideTemp=null
+
+let userEmailId
+
+const id=Symbol('190')
+const anotherId=Symbol('190')
+// console.log(id===anotherId);
+
+
+const bigNumber=82292984928248n
+
+//Non- Primitive ( Reference)
+//Arrays,Objects,functions
+
+const Heros=['shaktiman','ironman','golu'];
+
+let myObj={
+    name:"Sanket",
+    age:19,
+}
+
+
+const myFucntion = function(){
+    console.log("Hello world");
+}
