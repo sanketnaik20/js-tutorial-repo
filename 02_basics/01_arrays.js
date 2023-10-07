dd3a93612cd1c6de5b537c1e7d53e1ba64ce79ab
sanketnaik20 <64030169+sanketnaik20@users.noreply.github.com>
2023-10-07 17:57:59 +0000

    array_01

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,41 @@
+// array
+
+
+const myArr=[0,1,2,3,4];
+const myHeroes=["Shaktiman"]
+
+const myArray= new Array(1,2,3,4,5);
+// console.log(myArr[1]);
+
+
+/// array methods
+
+
+// myArr.push(6);
+// myArr.push(7);
+// myArr.pop();
+// myArr.unshift(9);
+// myArr.shift();
+
+
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(1))
+
+
+// const newArray= myArr.join()
+// 
+
+
+//slice and spice
+
+console.log("A",myArr);
+
+
+const myNewArr1=myArr.slice(1,3)
+
+console.log(myNewArr1);
+console.log("B",myArr);
+
+const myNewArr2=myArr.splice(1,3)
+console.log("C",myArr);
+console.log(myNewArr2);
